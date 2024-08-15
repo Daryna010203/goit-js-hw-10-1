9f62e9dd5e9af7add83471644e3043cd5fe4d183
Daryna Kurytsyna <152505147+Daryna010203@users.noreply.github.com>
2024-08-15 12:17:30 +0200

    update

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,7 +1,40 @@
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 
-const formInfo = document.querySelector('.form');
-formInfo.firstElementChild.classList.add('js-delay');
+const formInfoSnackbar = document.querySelector('.form');
+formInfoSnackbar.firstElementChild.classList.add('js-delay');
 const delayInput = document.querySelector('form label input');
 delayInput.classList.add('js-delay-input');
+
+
+function onFormSubmit (event) {
+    event.preventDefault();
+  
+        const delay = formInfoSnackbar.elements.delay.value;
+        const promise = new Promise((resolve, reject) => {
+        setTimeout(() => {
+        
+            event.preventDefault();
+            
+            if (formInfoSnackbar.elements.state.value === "fulfilled"){
+                resolve(delay)                
+            } else {
+                reject(delay)
+            }
+            
+        }, delay) 
+        
+    });
+    
+    promise.then(result => {
+        iziToast.success({
+            title: "Ok",
+            message: `Fulfilled promise in ${delay} ms`,
+            position: "topRight"});            ;
+    }).catch(error => iziToast.error({
+        title: "Error",
+        message: `Rejected promise in ${delay} ms`,
+        position: "topRight"}));
+    }
+   
+formInfoSnackbar.addEventListener('submit', onFormSubmit)
